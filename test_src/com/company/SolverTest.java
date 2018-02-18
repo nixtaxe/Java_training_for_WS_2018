5e216a5cf9285a4b6fa6148dbd8153eecc202a29
nixtaxe <fenix_dz@mail.ru>
2018-02-18 11:50:37 +1000

    Added some Solver class tests

---
+++ test_src/com/company/SolverTest.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+
+import static org.junit.Assert.*;
+
+public class SolverTest {
+    private double eps = 0.00001;
+
+    @Test
+    public void testXEqualsF() {
+        Solver s = new Solver(2, new double[][]{{1, 0},
+                                                         {0, 1}});
+        double[] res = s.getSolve(new double[]{-0.5,
+                                                11});
+        assertArrayEquals(new double[]{-0.5, 11}, res, eps);
+    }
+
+    @Test
+    public void testSimpleMatrixTransformation(){
+        Solver s = new Solver(2, new double[][]{{2, 1},
+                                                         {4, 7}});
+        double[] res = s.getSolve(new double[] {-1,
+                                                13});
+        assertArrayEquals(new double[]{-2, 3}, res, eps);
+    }
+}
