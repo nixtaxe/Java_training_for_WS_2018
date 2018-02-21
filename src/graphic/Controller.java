8b4a81267ad013ad043b4ff55e9346fd547df972
nixtaxe <fenix_dz@mail.ru>
2018-02-21 10:22:01 +1000

    Added Controller initialization, deleted unnecessary files

--- src/graphic/Controller.java
+++ src/graphic/Controller.java
@@ -3,54 +3,100 @@ package graphic;
 import javafx.fxml.FXML;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.stage.FileChooser;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 
-import static java.lang.Double.isNaN;
-
-public class Controller {
-//    private final FileChooser openFileChooser;
+public class Controller implements Initializable{
+    private FileChooser fileChooser;
     private InputStream inp;
-    private int n;
-    private int m;
+    public Spinner spinner;
+    public LineChart chart;
+    private double startX, endX;
     private boolean isEditingChart = false;
     private Model model;
 
-//    {
-//        openFileChooser = new FileChooser();
-//        openFileChooser.setInitialDirectory(new File(Paths.get(".").toAbsolutePath().normalize().toString()));
-//        openFileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("txt"));
-//    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        fileChooser = new FileChooser();
+        configureFileChooser();
 
-    @FXML
-    private void selectInputFile(ActionEvent event){
+        InputStream inp = this.getClass().getResourceAsStream("input.txt");
+        model = new Model(inp);
 
+        fillChart();
+        fillSpinner();
+    }
+
+    private void configureFileChooser() {
+        fileChooser.setInitialDirectory(new File(Paths.get(".").toAbsolutePath().normalize().toString()));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("TXT","*.txt"));
+    }
+
+    private void fillChart() {
+        chart.getData().removeAll(chart.getData());
+
+        XYChart.Series points = new XYChart.Series();
+        XYChart.Series approxPolynom = new XYChart.Series();
+
+        chart.getData().add(points);
+        chart.getData().add(approxPolynom);
+
+        double[] X = model.getX();
+        double[] Y = model.getY();
+
+        for (int i = 0; i < X.length; ++i)
+            points.getData().add(new XYChart.Data<>(X[i], Y[i]));
+
+        Arrays.sort(X);
+        double minX = X[0];
+        double maxX = X[X.length - 1];
+        double borderWidth = (maxX - minX)/4;
+        double delta = 0.05;
+
+        for (double x = minX - borderWidth; x <= maxX + borderWidth; x += delta)
+            approxPolynom.getData().add(new XYChart.Data<>(x, model.getApproxValue(x)));
+    }
+
+    private void fillSpinner() {
+        int maxDeg = model.getMaxApproxDegree();
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, maxDeg, maxDeg);
+        spinner.setValueFactory(valueFactory);
     }
 
     @FXML
-    private void changeApproxDegree(ActionEvent event){
-        double x = Integer.parseInt(((Spinner)event.getSource()).getPromptText());
-        if (isNaN(x) || n >= m)
-            //Заглушка. Должно вызываться исключение
-            n = 1;
-        else
-            n = (int)x;
+    private void selectInputFile(ActionEvent event) throws Exception{
+        File file = this.fileChooser.showOpenDialog(null);
+        if (file != null){
+            InputStream inp = new FileInputStream(file);
+            this.model = new Model(inp);
+            fillSpinner();
+            fillChart();
+        }
     }
 
     @FXML
-    private void startApprox(ActionEvent event){
-        System.out.print("Approx works");
+    private void startApprox(ActionEvent event) throws Exception{
+        if (model == null)
+            return;
+        int approx_deg = (int)spinner.getValue();
+        model.setApprox(approx_deg);
+        fillChart();
     }
 
     @FXML
     private void switchTo(ActionEvent event){
-
+        //...
     }
 }
